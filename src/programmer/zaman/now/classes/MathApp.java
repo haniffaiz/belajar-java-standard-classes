a0215e6ea649aa175d2a7ef8acacc744caece85f
Hanif Faiz Hidayat <grey.hanif@gmail.com>
2023-11-03 05:23:23 +0700

    math class

---
+++ src/programmer/zaman/now/classes/MathApp.java
@@ -0,0 +1,11 @@
+package programmer.zaman.now.classes;
+
+public class MathApp {
+    public static void main(String[] args) {
+
+        var min = Math.min(100, 200);
+        System.out.println(min);
+
+        System.out.println(Math.PI);
+    }
+}

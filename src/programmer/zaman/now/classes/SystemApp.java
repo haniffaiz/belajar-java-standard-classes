99c7fee9e4f812121912f7f4d0262ec00ef97e9d
Hanif Faiz Hidayat <grey.hanif@gmail.com>
2023-11-03 05:39:52 +0700

    System class

---
+++ src/programmer/zaman/now/classes/SystemApp.java
@@ -0,0 +1,11 @@
+package programmer.zaman.now.classes;
+
+public class SystemApp {
+    public static void main(String[] args) {
+        System.out.println(System.currentTimeMillis());
+        System.out.println(System.nanoTime());
+        System.out.println(System.getenv());
+
+
+    }
+}

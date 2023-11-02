1e49416fa5334aa5cc89f645bd955253e45c3709
Hanif Faiz Hidayat <grey.hanif@gmail.com>
2023-11-03 06:01:35 +0700

    Random class

---
+++ src/programmer/zaman/now/classes/RandomApp.java
@@ -0,0 +1,15 @@
+package programmer.zaman.now.classes;
+
+import java.util.Random;
+
+public class RandomApp {
+    public static void main(String[] args) {
+        Random random = new Random();
+
+        for(var i = 0; i < 1000; i++){
+            int value = random.nextInt(1000);
+
+            System.out.println(value);
+        }
+    }
+}
